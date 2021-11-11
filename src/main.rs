155bf04a414c52b895f2e6d157814bbe624d870a
vidur2 <vmod2005@gmail.com>
2021-11-11 10:01:51 -0500

    Alt branch

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,32 @@
-#[cxx::bridge]
+mod rust_implementation;
+
+use cxx::{type_id, ExternType};
+
+unsafe impl ExternType for rust_implementation::TimedRobot{
+    type Id = type_id!("wpilib_rust_impl:TimedRobot");
+    type Kind = cxx::Kind::Trivial;
+}
+
+#[cxx::bridge(namespace="wpilib_rust_impl")]
 mod ffi{
     unsafe extern "C++"{
         include!("wpilib_rs/header_files/Robot.h");
 
-        type Robot;
+        type TimedRobot = crate::rust_implementation::TimedRobot;
     }
+
 }
 
-use crate::ffi::Robot;
+use ffi::TimedRobot;
 
-impl Robot{
+impl TimedRobot{
     fn robot_init(){
-        // Robot init
+        println!("Shit")
     }
     fn robot_periodic(){
-        // Robot Periodic
-    }
-    fn autonomous_init(){
-        // autonomous init
-    }
-    fn autonomous_periodic(){
-        // autonomous periodic
-    }
-    fn teleop_init(){
-        // teleop init
-    }
-    fn teleop_periodic(){
-        // teleop periodic
+        println!("Shit")
     }
 }
-
-// impl TimedRobot for Robot {
-//     fn robot_init(){
-//         println!("Shit")
-//     }
-//     fn robot_periodic(){
-//         println!("Shit")
-//     }
-// }
 fn main() {
     println!("Hello World")
 }
