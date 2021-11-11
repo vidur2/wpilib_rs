f214f52456a05d3fd25b598874e0ba4d5bf06d32
vidur2 <vmod2005@gmail.com>
2021-11-11 08:50:12 -0500

    Working implementation?

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,15 @@
-trait TimedRobot{
-    fn robot_init();
-    fn robot_periodic();
-    fn autonomous_init();
-    fn autonomous_periodic();
-    fn teleop_init();
-    fn teleop_periodic();
-}
-
-pub struct Robot{}
-
 #[cxx::bridge]
 mod ffi{
     unsafe extern "C++"{
         include!("wpilib_rs/Robot.h");
 
-        type TimedRobot;
-        pub fn robot_init();
-        pub fn robot_periodic();
-        pub fn autonomous_init();
-        pub fn autonomous_periodic();
-        pub fn teleop_init();
-        pub fn teleop_periodic();
-
+        pub type TimedRobot;
     }
 }
 
-impl TimedRobot for Robot {
+use crate::ffi::TimedRobot;
+
+impl TimedRobot{
     fn robot_init(){
         // Robot init
     }
